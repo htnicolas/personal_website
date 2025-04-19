afe903016ef8fcfece50ab6ea891306bb5be03c2
Nicolas HT <nutella50g@gmail.com>
2025-04-19 16:28:37 -0700

    github action, astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,7 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   // Your site config here
-  site: 'https://nico-portfolio.com', // Replace with your actual domain
+  site: 'https://htnicolas.github.io/',
+  base: 'personal_website',
+  output: 'static',
 });
