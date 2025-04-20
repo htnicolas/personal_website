e48d60e072214aacf5bc58c07ce3ae37efbfbe27
Nicolas HT <nutella50g@gmail.com>
2025-04-19 17:19:57 -0700

    remove image import in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 import { defineConfig } from 'astro/config';
-import image from '@astrojs/image';
 
 export default defineConfig({
   // Your site config here
   site: 'https://htnicolas.github.io/',
   base: 'personal_website',
   output: 'static',
-  integrations: [image()],
 });
